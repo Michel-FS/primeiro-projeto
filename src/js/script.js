1fd9dc358c1dc26c522eb508b8a666b855fc049b
Michel-FS <michelfreitassantos@gmail.com>
2023-05-13 21:55:47 -0300

    Add files via upload

---
+++ src/js/script.js
@@ -0,0 +1,15 @@
+const botaoAlterarTema = document.getElementById('botao-alterar-tema');
+const body = document.querySelector('body');
+const imgTema = document.querySelector('.imagem-botao')
+
+botaoAlterarTema.addEventListener('click', () => {
+    const modoEscuroEstaAtivo = body.classList.contains('modo-escuro');
+
+    body.classList.toggle("modo-escuro");
+
+    if (modoEscuroEstaAtivo) {
+        imgTema.setAttribute('src', './src/imagens/sun.png')
+    } else {      
+        imgTema.setAttribute('src', './src/imagens/moon.png')
+    }
+});
